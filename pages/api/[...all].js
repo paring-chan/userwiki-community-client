f369ddb1cf569921b68ae92adff4d7bc04ccd2ed
PIKOSTUDIO <68010770+parangee@users.noreply.github.com>
2020-09-08 11:42:25 +0900

    Update [...all].js

--- pages/api/[...all].js
+++ pages/api/[...all].js
@@ -2,6 +2,6 @@ import httpProxyMiddleware from "next-http-proxy-middleware";
 
 export default (req, res) => (
     httpProxyMiddleware(req, res, {
-        target: 'http://home.minibox.pw',
+        target: 'https://userwiki.xyz',
     })
 );
